f5e5684eba293f270115000f271f64dcaa566471
Slutishboy <83270202+Slutishboy@users.noreply.github.com>
2023-02-14 19:38:23 +0530

    live stream complete

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -30,7 +30,10 @@ class FeedScreen extends StatelessWidget {
             }
             return ListView.builder(
               itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) => PostCard( ));
+                itemBuilder: (context, index) => PostCard(
+                  snap:snapshot.data!.docs[index].data(),
+                  
+                 ));
           }),
     );
   }
--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/utils/colors.dart';
+import 'package:intl/intl.dart';
 
 class PostCard extends StatelessWidget {
-  const PostCard({super.key});
+  final snap;
+  const PostCard({super.key, required this.snap});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: mobileBackgroundColor,
-      padding: const EdgeInsets.symmetric(vertical: 10,horizontal:0),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
       child: Column(
         children: [
           Container(
@@ -18,8 +20,7 @@ class PostCard extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 20,
-                  backgroundImage: NetworkImage(
-                      "https://firebasestorage.googleapis.com/v0/b/sohoms-insta-clone.appspot.com/o/profilePics%2F3UH1tz5ca0ZYm4UfMu3DpP2iYq03?alt=media&token=dbe58430-f882-4e91-9e3a-eafa1d0a1640"),
+                  backgroundImage: NetworkImage(snap['profImage']),
                 ),
                 Expanded(
                     child: Padding(
@@ -29,7 +30,7 @@ class PostCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "Ankana",
+                        snap['username'],
                         style: TextStyle(fontWeight: FontWeight.bold),
                       )
                     ],
@@ -38,138 +39,144 @@ class PostCard extends StatelessWidget {
                 IconButton(
                     onPressed: () {
                       showDialog(
-                          context: context,
-                          builder: (context) => Dialog(
-                                child: ListView(
-                                  padding:const  EdgeInsets.symmetric(vertical: 16),
-                                  shrinkWrap: true,
-                                  children: [
-                                    'Delete',
-                                  ].map(
-                                    (e) =>InkWell(
-                                    onTap: (){},
+                        context: context,
+                        builder: (context) => Dialog(
+                          child: ListView(
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            shrinkWrap: true,
+                            children: [
+                              'Delete',
+                            ]
+                                .map(
+                                  (e) => InkWell(
+                                    onTap: () {},
                                     child: Container(
                                       padding: const EdgeInsets.symmetric(
-                                        vertical: 12,
-                                        horizontal: 16
-                                      ),
+                                          vertical: 12, horizontal: 16),
                                       child: Text(e),
-
                                     ),
-
-                                  ),).toList(),
                                   ),
-                                
-                                ),
-                              );
+                                )
+                                .toList(),
+                          ),
+                        ),
+                      );
                     },
                     icon: Icon(Icons.more_vert))
               ],
             ),
             //Image Section
-            
           ),
-          Container(padding:EdgeInsets.only(top: 5),),
+          Container(
+            padding: EdgeInsets.only(top: 5),
+          ),
           SizedBox(
-              height: MediaQuery.of(context).size.height*0.40,
-              width: double.infinity,
-              child: Image.network('https://firebasestorage.googleapis.com/v0/b/sohoms-insta-clone.appspot.com/o/profilePics%2F3UH1tz5ca0ZYm4UfMu3DpP2iYq03?alt=media&token=dbe58430-f882-4e91-9e3a-eafa1d0a1640',fit: BoxFit.cover,),
-              ),
-              // LIke Comment section
-
-            Row(
-              children: [
-                IconButton(
-                  onPressed: (){},
-                   icon: const Icon(Icons.favorite,color: Colors.red,
-                   ),
-                  ),
-                IconButton(
-                  onPressed: (){},
-                   icon: const Icon(Icons.comment_outlined,
-                   ),
-                  ),
-                IconButton(
-                  onPressed: (){},
-                   icon: const Icon(Icons.send,
-                   ),
-                  ),
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: IconButton(
-                        icon:const Icon(Icons.bookmark_border),
-                        onPressed: (){},
-                      ),
-                  )
-                  )
-              ],
+            height: MediaQuery.of(context).size.height * 0.40,
+            width: double.infinity,
+            child: Image.network(
+              snap['postUrl'],
+              fit: BoxFit.cover,
             ),
-            //Description and number of cmments
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16
+          ),
+          // LIke Comment section
+
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  DefaultTextStyle(
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      fontWeight: FontWeight.w800
-                    ),
-                    child: Text('1,231 likes',style: Theme.of(context).textTheme.bodyMedium,
-                    ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.comment_outlined,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.send,
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomRight,
+                child: IconButton(
+                  icon: const Icon(Icons.bookmark_border),
+                  onPressed: () {},
+                ),
+              ))
+            ],
+          ),
+          //Description and number of cmments
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(fontWeight: FontWeight.w800),
+                  child: Text(
+                    '${snap['likes'].length} likes',
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
-                  Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.only(top: 8),
-                    child: RichText(
-                      text:TextSpan(
-                        style: const TextStyle(
-                          color: primaryColor
-                        ),children: [
-                          TextSpan(
-                          text: 'Ankana',
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(top: 8),
+                  child: RichText(
+                      text: TextSpan(
+                          style: const TextStyle(color: primaryColor),
+                          children: [
+                        TextSpan(
+                          text: snap['username'],
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                          TextSpan(
-                          text: '  Sexy AFFFF !!!!...',
+                        TextSpan(
+                          text: ' ${snap['description']}',
                           // style: TextStyle(
                           //   fontWeight: FontWeight.bold,
                           // ),
                         ),
-                        ]
-                      )
-                      ),
-                  ),
-                  InkWell(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 4),
-                      child: Text('View all 200 comments',style: TextStyle(
+                      ])),
+                ),
+                InkWell(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      'View all 200 comments',
+                      style: TextStyle(
                         fontSize: 16,
                         color: secondaryColor,
-                  
-                  
-                      ),),
+                      ),
                     ),
                   ),
-                  Container(
-                      padding: EdgeInsets.symmetric(vertical: 4),
-                      child: Text('22 December',style: TextStyle(
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      DateFormat.yMMMd().format(snap['datePublished'].toDate(),),
+                      style: TextStyle(
                         fontSize: 14,
                         color: secondaryColor,
-                  
-                  
-                      ),))
-
-                ],
-              ),
-            )
+                      ),
+                    ))
+              ],
+            ),
+          )
         ],
       ),
     );
   }
+
+ 
 }
